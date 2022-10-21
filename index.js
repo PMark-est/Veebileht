19e34e0f0b92728168536cf7d77ef67027ee29b7
PMark-EST <markopeedosk@gmail.com>
2022-10-21 23:08:06 +0300

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var col = document.getElementsByClassName("btn");
+var i;
+
+for (i = 0; i < col.length; i++){
+    col[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        console.log(content.style.display);
+        if(content.style.display == "block"){
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    }); 
+}
