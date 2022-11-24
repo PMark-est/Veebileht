1c352ad3596d5aad07b2f38ae2a958cab36a886b
PMark-EST <markopeedosk@gmail.com>
2022-11-24 23:03:26 +0200

    Kodulehe lõplik disain

--- nupud.js
+++ nupud.js
@@ -1,5 +1,5 @@
 // Nuppule vajutades see kuvab vastava sisu
-var col = document.getElementsByClassName("btn");
+/*var col = document.getElementsByClassName("btn");
 var i;
 
 for (i = 0; i < col.length; i++){
@@ -7,10 +7,13 @@ for (i = 0; i < col.length; i++){
         this.classList.toggle("active");
         var content = this.nextElementSibling;
         console.log(content.style.display);
-        if(content.style.display == "block"){
-            content.style.display = "none";
+        if(content.style.visibility == "visible"){
+            content.style.visibility = "hidden";
+            content.style.height = 0;
         } else {
-            content.style.display = "block";
+            content.style.visibility = "visible";
+            content.style.height = "70vh";
         }
     }); 
 }
+*/
